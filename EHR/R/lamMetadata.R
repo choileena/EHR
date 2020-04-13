687e327cfd36aa168676f8d2c496c5f292fee511
Elizabeth McNeer <elizabeth.mcneer@vumc.org>
2020-04-13 11:09:02 -0500

    add example data and documentation

---
+++ EHR/R/lamMetadata.R
@@ -0,0 +1,17 @@
+#' Example of Metadata for Lamotrigine Data
+#'
+#' An example of the metadata needed for the `makeDose` and `collapse` functions.
+#'
+#' @format A data frame with 5 observations on the following variables.
+#' \describe{
+#'   \item{filename}{A character vector, filename for the clinical note}
+#'   \item{pid}{A character vector, patient ID associated with the filename}
+#'   \item{date}{A character vector, date associated with the filename}
+#'   \item{note}{A character vector, note ID associated with the filename}
+#' }
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(lamMetadata)
---
+++ EHR/R/lamParseOutput.R
@@ -0,0 +1,22 @@
+#' Example of Lamotrigine Output from `parseMedExtractR`
+#'
+#' The output after running `parseMedExtractR` on 4 example clinical notes.
+#'
+#' @format A data frame with 10 observations on the following variables.
+#' \describe{
+#'   \item{filename}{A character vector, filename for the clinical note}
+#'   \item{drugname}{A character vector, drug name extracted from the clinical note along with start and stop positions}
+#'   \item{strength}{A character vector, strengths extracted from the clinical note along with start and stop positions}
+#'   \item{dose}{A character vector, dose amounts extracted from the clinical note along with start and stop positions}
+#'   \item{route}{A character vector, routes extracted from the clinical note along with start and stop positions}
+#'   \item{freq}{A character vector, frequencies extracted from the clinical note along with start and stop positions}
+#'   \item{dosestr}{A character vector, dose intakes extracted from the clinical note along with start and stop positions}
+#'   \item{dosechange}{A character vector, dose change keywords extracted from the clinical note along with start and stop positions}
+#'   \item{lastdose}{A character vector, last dose times extracted from the clinical note along with start and stop positions}
+#' }
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(lamParseOutput)
