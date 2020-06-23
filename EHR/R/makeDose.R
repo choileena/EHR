c4345f4450cc4900e12a48d5c1f3d3908bfab78a
Cole Beck <cole.beck@vumc.org>
2020-06-23 11:20:15 -0500

    Update makeDose.R
    include dose "five"

--- EHR/R/makeDose.R
+++ EHR/R/makeDose.R
@@ -176,6 +176,7 @@ makeDose <- function(x, noteMetaData, naFreq = 'most') {
   cdose <- sub('two', 2, cdose)
   cdose <- sub('three', 3, cdose)
   cdose <- sub('four', 4, cdose)
+  cdose <- sub('five', 5, cdose)
   cdose <- sub('half', 0.5, cdose, fixed = TRUE)
   cdose <- sub('1-2', 1.5, cdose, fixed = TRUE)
   cdose <- sub('1-1/2', 1.5, cdose, fixed = TRUE)
