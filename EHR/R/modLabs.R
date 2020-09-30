6c79cd0122918488d584742ff6c6246dd55fce34
Nathan T. James <nj1154@gmail.com>
2020-09-30 18:40:29 -0500

    update docs & add example for run_Labs

--- EHR/R/modLabs.R
+++ EHR/R/modLabs.R
@@ -4,19 +4,28 @@
 #'
 #' @param lab.path filename of a lab file (stored as RDS)
 #' @param lab.select columns to select
-#' @param lab.mod.list list of expressions, giving modifications to make
+#' @param lab.mod.list list of expressions giving modifications to make;
+#'  passed to \code{\link{dataTransformation}}
 #'
 #' @return lab data set
+#'
+#' @examples 
+#' \dontrun{
+#' lab_data <- data.frame(mod_id=rep(1:3,each=3),
+#'                        date=rep(c("01/12/17","05/05/18","11/28/16"),each=3),
+#'                        time=rep(c("1:30","2:30","3:30"),3),
+#'                        creat=rnorm(9,0.5,0.05))
+#' 
+#' saveRDS(lab_data, 'lab_data.rds')
+#' 
+#' run_Labs('lab_data.rds', lab.mod.list=list(log_creat=expression(log(creat))))
+#' }
+#' 
 #'
 #' @export
 
 run_Labs <- function(lab.path, lab.select, lab.mod.list) {
-# # read and transform data
   lab.in <- readRDS(lab.path)
-#   lab.in <- read.csv(lab.path)
-#   # convert MRN into mod_id
-#   xx <- get(getOption('pkxwalk'), .GlobalEnv)
-#   lab.in[,'mod_id'] <- xx[match(lab.in[,'Subject.mrn'], xx[,'mrn']), 'mod_id']
   lab <- dataTransformation(lab.in, modify = lab.mod.list)
   lab[,lab.select]
 }
