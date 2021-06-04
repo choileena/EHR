71c0df6df99a30b88c39ecdc903beb74fbfb10c8
choileena <naturechoi@gmail.com>
2021-06-04 18:57:00 -0500

    Update modPK.R

--- EHR/R/modPK.R
+++ EHR/R/modPK.R
@@ -23,6 +23,8 @@
 #' @param date.format output format for \sQuote{date} variable
 #' @param date.tz output time zone for \sQuote{date} variable
 #'
+#' @details See EHR Vignette for Structured Data
+#'
 #' @return PK data set
 #'
 #' @examples 
