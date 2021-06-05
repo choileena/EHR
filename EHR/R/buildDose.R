9071da6c5f73b635dca73896407cfbe49caa8d91
choileena <naturechoi@gmail.com>
2021-06-04 22:43:05 -0500

    Update buildDose.R

--- EHR/R/buildDose.R
+++ EHR/R/buildDose.R
@@ -38,8 +38,8 @@
 #'  threshold puts an upper bound on the number of entity pairings to prevent the function from stalling in such cases.
 #' }
 #' If none of the optional arguments are specified, then the \code{buildDose} process uses the default option values specified in the EHR
-#' package documentation.
-#'
+#' package documentation. 
+#' See EHR Vignette for Extract-Med and Pro-Med-NLP as well as Dose Building Using Example Vanderbilt EHR Data for details.
 #' For additional details, see McNeer, et al. 2020.
 #'
 #' @param dat data.table object from the output of \code{\link{parseMedExtractR}},
@@ -57,8 +57,6 @@
 #' algorithm taking a long time to finish. Alternatively set the\cr
 #' \sQuote{ehr.greedy_threshold} option, which defaults to 1e8.
 #' @param checkForRare Indicate if rare values for each entity should be found and displayed.
-#'
-#' @details See EHR Vignette for Extract-Med and Pro-Med-NLP as well as Dose Building Using Example Vanderbilt EHR Data for details.
 #'
 #' @return A data.frame object that contains columns for filename (of the clinical note, inherited from the
 #' parse output object \code{dat}), drugname, strength, dose, route, freq, duration, and drugname_start.
